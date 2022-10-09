ff3426d337535363850000065f21ff8d092519ea
Nisarg01-01 <82572011+Nisarg01-01@users.noreply.github.com>
2022-10-10 00:51:17 +0530

    updated backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,5 @@
-console.log("this file contains code for the backend")
+const express = require("express");
+const app = express();
+const port = 4000;
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
